58d3917e32821148f31bd41b037bae4534544048
Adrian Frith <adrianf@da.org.za>
2021-11-07 00:02:56 +0200

    Apply standard style

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import path from 'path'
 import render from 'koa-ejs'
 import serve from 'koa-static'
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
 
 if (process.env.NODE_ENV === 'production') {
   winston.level = 'info'
 } else {
   winston.level = 'debug'
 }
-app.use(koaLogger(str => winston.info(str)));
+app.use(koaLogger(str => winston.info(str)))
 
 render(app, {
   root: path.join(__dirname, 'views'),
@@ -27,16 +27,16 @@ router.get('/', async ctx => {
   ctx.body = await ctx.render('index')
 })
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods())
 
-app.use(serve(__dirname + '/static'));
+app.use(serve(path.join(__dirname, 'static')))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function (err) {
   if (err) {
-    winston.error('Error starting server:', err);
-    process.exit(1);
+    winston.error('Error starting server:', err)
+    process.exit(1)
   }
 
-  winston.info('Server listening on port', port);
+  winston.info('Server listening on port', port)
 })
