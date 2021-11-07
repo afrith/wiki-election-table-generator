a766a221054363b8fff26f616ef3393d6a1d77b9
Adrian Frith <adrianf@da.org.za>
2021-11-07 14:01:39 +0200

    Add short table version

--- src/routes.js
+++ src/routes.js
@@ -45,7 +45,40 @@ router.get('/:election/:muni_code', async ctx => {
     getBallotTotals(muniCode, election.date),
     getTurnout(muniCode, election.date)
   ])
-  await ctx.render('table', { election, muniName, partyVotes, ballotTotals, turnout, formatDate, formatNumber, formatPercent })
+
+  const showShortLink = partyVotes.filter(row => row.total_seats === 0 && row.wikilink !== 'Independent candidates').length > 1
+
+  await ctx.render('table', { election, muniCode, muniName, showShortLink, partyVotes, ballotTotals, turnout, formatDate, formatNumber, formatPercent })
+})
+
+router.get('/:election/:muni_code/short', async ctx => {
+  const election = elections[ctx.params.election]
+  if (!election) return ctx.throw(404, `No such election as "${ctx.params.election}".`)
+  election.code = ctx.params.election
+
+  const muniCode = ctx.params.muni_code
+  const muniName = await getMuniName(muniCode, election.date)
+  if (!muniName) return ctx.throw(404, `No such municipality as "${muniCode}" in election "${ctx.params.election}".`)
+
+  const [partyVotes, ballotTotals, turnout] = await Promise.all([
+    getPartyVotes(muniCode, election.date),
+    getBallotTotals(muniCode, election.date),
+    getTurnout(muniCode, election.date)
+  ])
+
+  const visiblePartyVotes = partyVotes.filter(row => row.total_seats > 0 || row.wikilink === 'Independent candidates')
+  const otherPartyVotes = partyVotes.filter(row => row.total_seats === 0 && row.wikilink !== 'Independent candidates')
+
+  console.log(otherPartyVotes)
+  const others = otherPartyVotes.reduce((previous, current) => ({
+    ward_votes: previous.ward_votes + (current.ward_votes || 0),
+    pr_votes: previous.pr_votes + (current.pr_votes || 0),
+    total_votes: previous.total_votes + (current.total_votes || 0),
+    perc: previous.perc + (current.perc || 0),
+    count: previous.count + 1
+  }), { ward_votes: 0, pr_votes: 0, total_votes: 0, perc: 0, count: 0 })
+
+  await ctx.render('shorttable', { election, muniCode, muniName, partyVotes: visiblePartyVotes, others, ballotTotals, turnout, formatDate, formatNumber, formatPercent })
 })
 
 export default router
